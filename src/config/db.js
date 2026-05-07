474c8e2773b2b5912c6c7ad6dafb672bec41552d
Sang <sang.nguyen@tidadigi.com>
2026-05-07 21:24:17 +0700

    .

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
+let isConnected = false;
+
 const connectDB = async () => {
+    if (isConnected) {
+        return;
+    }
+
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
+        isConnected = !!conn.connections[0].readyState;
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1);
+        // process.exit(1); // Don't exit in serverless
     }
 };
 
