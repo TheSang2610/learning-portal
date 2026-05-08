e71ebbc7196726f872f2ceec7de51482b8a02bda
Sang <sangghom999@gmail.com>
2026-05-08 22:35:45 +0700

    5

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { createCourse, getCourses } = require('../controllers/courseController');
-const { protect, admin } = require('../middlewares/authMiddleware');
+
+const {
+    createCourse,
+    getCourses
+} = require('../controllers/courseController');
+
+const { protect } = require('../middlewares/authMiddleware');
 
 router.route('/')
     .get(getCourses)
-    .post(protect, createCourse); // Chỉ người đăng nhập mới tạo được
+    .post(protect, createCourse);
 
 module.exports = router;
