02a20b7841836faedbb41164132a0233156a22e3
Sang <sangghom999@gmail.com>
2026-05-20 00:17:35 +0700

    fix user

--- src/models/User.js
+++ src/models/User.js
@@ -41,7 +41,7 @@ const userSchema = new mongoose.Schema({
 
     phone: {
         type: String,
-        default: '',
+        default: null,
         unique: true,
         sparse: true
     },
@@ -53,7 +53,7 @@ const userSchema = new mongoose.Schema({
 
     googleId: {
         type: String,
-        default: '',
+        default: null,
         unique: true,
         sparse: true
     },
